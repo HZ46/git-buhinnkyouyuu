23bb7962b3117652b5de10da92192c3394e540c3
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-10-17 13:47:05 +0000

    test

--- app/controllers/department/cart_items_controller.rb
+++ app/controllers/department/cart_items_controller.rb
@@ -9,18 +9,24 @@ class Department::CartItemsController < ApplicationController
   def create
     @item = Item.find(params[:cart_item][:item_id])
     @cart_items = current_department.cart_items
-    
     if current_department.cart_items.where(item_id: @item.id).present?
+      print('=================')
       @cart_item = current_department.cart_items.find_by(item_id: @item.id)
       new_amount = @cart_item.amount + cart_item_params[:amount].to_i
       if @item.amount < new_amount
+        print(1111111111)
         redirect_back  fallback_location: root_path, alert: "在庫が足りません"
-      else  
+      else
         @cart_item.update(amount: new_amount)
+        print(@cart_item)
         redirect_to cart_items_path
       end
     else
+      print('-----------------')
+      print(@item.amount)
+      print(cart_item_params[:amount].to_i)
       if @item.amount >= cart_item_params[:amount].to_i
+        print(22222222222)
         # カートにアイテムを追加する
         @cart_item = current_department.cart_items.new(cart_item_params)
         @cart_item.item_id = @item.id
