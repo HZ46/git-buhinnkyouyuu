a484ddd5f5e8fb372122ac68189d0d0906f66c33
EC2 Default User <ec2-user@ip-172-31-25-255.ec2.internal>
2022-10-25 18:47:31 +0000

    test

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -6,7 +6,12 @@ class Admin::OrderDetailsController < ApplicationController
     @order_details = @order.order_details
     @order_detail.update(order_detail_params)
 
-    if @order_details.where(making_status: "pikking").count 
+    if @order_details.count == @order_details.where(making_status: "sold_out").count
+      @order.status = :cannot_start
+      @order.save
+    end
+
+    if @order_details.count == @order_details.where(making_status: "pikking").count
       @order.status = :accepting
       @order.save
     end
@@ -15,13 +20,13 @@ class Admin::OrderDetailsController < ApplicationController
        @order.status = :preparing_for_delivery
        @order.save
      end
-     
+
      if @order.order_details.count == @order_details.where(making_status: "deal_closed").count
        @order.status = :delivered
        @order.save
      end
-     
-     
+
+
     redirect_to admin_order_path(@order_detail.order.id)
    end
 
@@ -31,4 +36,10 @@ class Admin::OrderDetailsController < ApplicationController
     params.require(:order_detail).permit(:making_status)
   end
 end
+    #if @order_detail.status == "入金確認"  (一斉にステータス変えるコード)
+     # @order_details.each do |order_detail|
+      #  order_detail.making_status = "製作待ち"
+       # order_detail.save
+      #end
+
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,5 +2,5 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum making_status: { cannot_start: 0, pikking: 1, completed: 2 ,deal_closed: 3, sold_out: 99}
+  enum making_status: { before_start: 0, pikking: 1, completed: 2 ,deal_closed: 3, sold_out: 99}
 end
